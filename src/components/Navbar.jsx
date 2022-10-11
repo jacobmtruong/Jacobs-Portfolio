544cc2fda03e30a3e4c8f1f57895b592441320dd
Jacob Truong <jacob.truong@ymail.com>
2022-10-10 20:17:42 -0700

    resume pdf download

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,47 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import '../styles/navbar.css'
 import jmt from '../images/jmt.svg'
+import resume from '../images/Jacob_Truong_resume.pdf'
 
 const Navbar = () => {
     // Open In New Tab
-    const openInNewTab = url => {
-        window.open(url, '_blank', 'noopener,noreferrer');
-      };
+    // const openInNewTab = url => {
+    //     window.open(url, '_blank', 'noopener,noreferrer');
+    // };
 
-  return (
-    <>
-        <div className='navbar-container'>
-            <div className='navbar-small-container'>
-                <NavLink to='/projects'><img src={jmt} alt="jmt-logo" /></NavLink>
+    // download files
+    // const onClickResume = () => {
+    //     // using Java Script method to get PDF file
+    //     fetch('Jacob_Truong_resume.pdf').then(response => {
+    //         response.blob().then(blob => {
+    //             // Creating new object of PDF file
+    //             const fileURL = window.URL.createObjectURL(blob);
+    //             // Setting various property values
+    //             let alink = document.createElement('a');
+    //             alink.href = fileURL;
+    //             alink.download = 'Jacob_Truong_resume.pdf';
+    //             alink.click();
+    //         })
+    //     })
+    // }
 
-                <ul className='navbar-button-container'>
-                    <li><NavLink to='/projects' className={({ isActive }) => (isActive ? "navbar-btn-active" : "navbar-btn")}>Projects</NavLink></li>
-                    <li><NavLink to='/introduction' className={({ isActive }) => (isActive ? "navbar-btn-active" : "navbar-btn")}>About Me</NavLink></li>
-                    <li><NavLink onClick={() => openInNewTab('https://docs.google.com/document/d/1KSNwWAvoN168sRC1PUszAASQadhU6LyDTD5B_NVKO1c/edit?usp=sharing')} className="navbar-btn">Resume</NavLink></li>
-                    <li><NavLink to='/contact' className='navbar-btn-contact'>Contact</NavLink></li>
-                </ul>
+    return (
+        <>
+            <div className='navbar-container'>
+                <div className='navbar-small-container'>
+                    <NavLink to='/projects'><img src={jmt} alt="jmt-logo" /></NavLink>
+
+                    <ul className='navbar-button-container'>
+                        <li><NavLink to='/projects' className={({ isActive }) => (isActive ? "navbar-btn-active" : "navbar-btn")}>Projects</NavLink></li>
+                        <li><NavLink to='/introduction' className={({ isActive }) => (isActive ? "navbar-btn-active" : "navbar-btn")}>About Me</NavLink></li>
+                        <li><a href={resume} download="JacobTruong_resume.pdf" className="navbar-btn">Resume</a></li>
+                        <li><NavLink to='/contact' className='navbar-btn-contact'>Contact</NavLink></li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Navbar
