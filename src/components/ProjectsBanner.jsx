db2567054986d50937351cc2e072b7d3030e4f07
Jacob Truong <jacob.truong@ymail.com>
2022-10-11 11:25:39 -0700

    change projects layouts

--- src/components/ProjectsBanner.jsx
+++ src/components/ProjectsBanner.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../styles/projectsbanner.css'
-import arrow from '../images/arrow.svg'
 import { Link } from 'react-router-dom'
 
 const ProjectsBanner = () => {
@@ -8,19 +7,44 @@ const ProjectsBanner = () => {
         <div className='projects-banner-container'>
             <div className='projects-banner-small-container'>
 
-                <Link className='single-project-container'>
-                    <img src={require('../images/dmvrex.png')} alt="dmvrex-project" className='project-banner-img-dmv'/>
+                <div className='image-dmv'>
+                    <div className='project-text'>
+                        <p style={{fontSize: "36px"}}>DMV Rex</p>
+                        <p style={{fontSize: "18px", color: "#C3CEE1"}}>Python / Flask / MySQL</p>
+                    </div>
+                    <Link className='link'>Learn More</Link>
+                </div>
+
+                <div className='image-fitlab'>
+                    <div className='project-text'>
+                        <p style={{fontSize: "36px"}}>FitLab</p>
+                        <p style={{fontSize: "18px", color: "#C3CEE1"}}>React JS / Express / Mongoose</p>
+                    </div>
+                    <Link className='link'>Learn More</Link>
+                </div>
+
+
+                <div className='image-festlook'>
+                    <div className='project-text'>
+                        <p style={{fontSize: "36px"}}>FestLook</p>
+                        <p style={{fontSize: "18px", color: "#C3CEE1"}}>Java / Spring Boot / MySQL</p>
+                    </div>
+                    <Link className='link'>Learn More</Link>
+                </div>
+
+                {/* <Link className='single-project-container'>
+                    <img src={require('../images/dmvrex.png')} alt="dmvrex-project" className='project-banner-img-dmv' />
                     <div className='project-text-container'>
                         <p className='project-title'>DMV Rex</p>
-                        <img src={arrow} alt="arrow-view" className='project-arrow'/>
+                        <img src={arrow} alt="arrow-view" className='project-arrow' />
                     </div>
-                </Link>
-
+                </Link> */}
+{/* 
                 <Link className='single-project-container'>
                     <img src={require('../images/FitLab.png')} alt="dmvrex-project" className='project-banner-img-fitlab' />
                     <div className='project-text-container'>
                         <p className='project-title'>FitLab</p>
-                        <img src={arrow} alt="arrow-view" className='project-arrow'/>
+                        <img src={arrow} alt="arrow-view" className='project-arrow' />
                     </div>
                 </Link>
 
@@ -28,10 +52,9 @@ const ProjectsBanner = () => {
                     <img src={require('../images/FestLook.png')} alt="dmvrex-project" className='project-banner-img-festlook' />
                     <div className='project-text-container'>
                         <p className='project-title'>FestLook</p>
-                        <img src={arrow} alt="arrow-view" className='project-arrow'/>
+                        <img src={arrow} alt="arrow-view" className='project-arrow' />
                     </div>
-                </Link>
-
+                </Link> */}
             </div>
         </div>
     )
