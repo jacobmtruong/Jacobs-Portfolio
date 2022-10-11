ce3ff30a11ec6a665fc158b83be2627f08e3dc7b
Jacob Truong <jacob.truong@ymail.com>
2022-10-11 07:50:26 -0700

    changing vh

--- src/components/FooterPortfolio.jsx
+++ src/components/FooterPortfolio.jsx
@@ -1,5 +1,5 @@
-import {useState} from 'react'
-import {Link} from 'react-router-dom'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 import '../styles/footer.css'
 import jmtsmall from '../images/jmt-small.svg'
 import github from '../images/github.svg'
@@ -12,29 +12,31 @@ const FooterPortfolio = () => {
     // Open a new tab with URL
     const openInNewTab = url => {
         window.open(url, '_blank', 'noopener,noreferrer');
-      };
+    };
 
-      const [contact, setContact] = useState(false)
+    const [contact, setContact] = useState(false)
 
-      const toggleContact = () => {
-          setContact(!contact)
-      }
+    const toggleContact = () => {
+        setContact(!contact)
+    }
 
-  return (
-    <div className='footer-container'>
-        <div className='left-nest'>
-            <img src={jmtsmall} alt="jmt-icon" />
-            <p className='footer-legal'>© 2022 Jacob Minh Truong</p>
-        </div>
-        <p className='middle-nest'>Let's Connect</p>
-        <div className='right-nest'>
-            <Link onClick={() => openInNewTab('https://github.com/jacobmtruong')}><img src={github} alt="github-icon" /></Link>
-            <Link onClick={() => openInNewTab('https://www.linkedin.com/in/jacob-truong/')}><img src={linkedin} alt="linked-icon" /></Link>
-            <Link onClick={toggleContact}><img src={email} alt="email-icon"/></Link>
-        </div>
-        {contact && (<ContactForm toggleContact = {toggleContact}/>)}
-    </div>
-  )
+    return (
+        <footer>
+            <div className='footer-container'>
+                <div className='left-nest'>
+                    <img src={jmtsmall} alt="jmt-icon" />
+                    <p className='footer-legal'>© 2022 Jacob Minh Truong</p>
+                </div>
+                <p className='middle-nest'>Let's Connect</p>
+                <div className='right-nest'>
+                    <Link onClick={() => openInNewTab('https://github.com/jacobmtruong')}><img src={github} alt="github-icon" /></Link>
+                    <Link onClick={() => openInNewTab('https://www.linkedin.com/in/jacob-truong/')}><img src={linkedin} alt="linked-icon" /></Link>
+                    <Link onClick={toggleContact}><img src={email} alt="email-icon" /></Link>
+                </div>
+                {contact && (<ContactForm toggleContact={toggleContact} />)}
+            </div>
+        </footer>
+    )
 }
 
 export default FooterPortfolio
