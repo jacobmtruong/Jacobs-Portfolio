5955c3008811f9207a18f31d58eb669a7736e624
Jacob Truong <jacob.truong@ymail.com>
2023-02-10 06:57:09 -0800

    banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import '../styles/banner.css'
+import React from "react";
+import "../styles/banner.css";
 
 const Banner = () => {
   return (
-    <div className='banner-container'>
-        <p className='name-text'>Hi, I'm Jacob!</p>
-        <div className='small-text'>
-            <p>I’m a Full-Stack Developer, based in the SF Bay Area</p>
-            <p className='second-small-text'>Let's make it happen on the Internet!</p>
-        </div>
-        
+    <div className="banner-container">
+      <p className="name-text">Hi, I'm Jacob!</p>
+      <div className="small-text">
+        <p>I’m a Full-Stack Developer, based in the SF Bay Area</p>
+        <p className="second-small-text">
+          Making the world a better place through creative software development,
+          culture and fun vibes. Let's make it happen on the Internet!
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
