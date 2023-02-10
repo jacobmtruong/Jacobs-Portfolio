29c633f02ea9f779d18ae24f263c152b7241145e
Jacob Truong <jacob.truong@ymail.com>
2023-02-10 09:42:28 -0800

    css

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -10,8 +10,8 @@ const Banner = () => {
           I’m a Full-Stack Developer, based in the SF Bay Area
         </p>
         <p className="second-small-text">
-          Making the world a better place through creative software
-          development,culture and fun vibes
+          Making the world a better place through creative software development,
+          culture, and fun vibes
         </p>
       </div>
     </div>
